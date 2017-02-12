bfba75e5486ae0c1c48689ea32fb8659b2509889
simpart <taria.ki4m@gmail.com>
2017-02-12 18:40:22 +0900

    support mofron v0.6.x

---
+++ src/common.js
@@ -0,0 +1,56 @@
+/**
+ * @file common.js
+ */
+mofron.event.Common = class extends mofron.Event {
+    
+    constructor (fnc, prm) {
+        try {
+            super(fnc,prm);
+            this.name('Common');
+            this.evt_name = null;
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    eventName (nm) {
+        try {
+            if (undefined === nm) {
+                return this.evt_name;
+            }
+            if ('string' !== (typeof nm)) {
+                throw new Error('invalid parameter');
+            }
+            this.evt_name = nm;
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    eventConts () {
+        try {
+            var cbf = this.func;
+            var cbp = this.parm;
+            
+            if ((null      === this.evt_name) ||
+                (undefined === this.target.getRawDom()[this.evt_name])) {
+                throw new Error('invalid event name');
+            }
+            this.target.getRawDom()[this.evt_name] = function () {
+                try {
+                    if (null != cbf) {
+                        cbf(cbp);
+                    }
+                } catch (e) {
+                    console.error(e.stack);
+                    throw e;
+                }
+            };
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+}

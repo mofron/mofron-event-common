f3d64a04dc69da1c33b876098615161a0e600e98
simpart <simpart>
2019-07-15 11:51:32 +0900

    add comment

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # mofron-event-common
+[mofron](https://mofron.github.io/mofron/) is module based frontend framework.
+
+common event for mofron
+
+set 'addEventListener' to target dom
+
+
+# Install
+```
+npm install mofron mofron-event-common
+```
+
+# Parameter
+
+|Simple<br>Param | Parameter Name | Type | Description |
+|:--------------:|:---------------|:-----|:------------|
+|◯ | handler | function/array | function: event function|
+|  |         |                | array:[event func, event param] |
+|◯ | type | string | event type |
+
--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 /**
- * @file mofron-event-common.js
+ * @file mofron-event-common/index.js
  * @brief common event for mofron
+ *        set 'addEventListener' to target dom
  * @author simpart
  */
 const mf = require('mofron');
 
 mf.event.Common = class extends mf.Event {
     
+    /**
+     * initialize common event
+     * 
+     * @param (function/array) function: event function
+     *                         array: [event func, event param]
+     * @param (string) 'type' parameter
+     * @pmap handler,type
+     * @type private
+     */
     constructor (po, p2) {
         try {
             super();
@@ -21,9 +31,10 @@ mf.event.Common = class extends mf.Event {
     
     /**
      * event type
-     *
+     * 
      * @param (string) event type
      * @return (string) event type
+     * @type parameter
      */
     type (prm) {
         try { return this.member("type", "string", prm); } catch (e) {
@@ -32,6 +43,12 @@ mf.event.Common = class extends mf.Event {
         }
     }
     
+    /**
+     * event contents
+     *
+     * @param (object) dom object
+     * @type private
+     */
     contents (tgt_dom) {
         try {
             let evt_obj = this;
